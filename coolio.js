b580199051ce3c37ee0128317fe89220593e02e5
oliviathecook <41088847+oliviathecook@users.noreply.github.com>
2018-07-25 15:15:00 -0700

    Add files via upload

---
+++ coolio.js
@@ -0,0 +1,9 @@
+// function formChanged() {
+//     var firstName = document.getElementsByName("firstname")[0].value;
+//     var lastName = document.getElementsByName("lastname")[0].value;
+//     var userName = document.getElementsByName("user")[0].value;
+//     var password = document.getElementsByName("pass")[0].value;
+//     var repeatPass = document.getElementsByName("rpass")[0].value;
+// }
+
+// var firstName = document.getElementsByName("firstname")[0].value;
